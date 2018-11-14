0b18be438871ccea2fa8fa7e472db360b0ded20d
MourlonG <mourlonguillaume@gmail.com>
2018-11-14 11:43:01 +0100

    init todos

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ const port = 8000
 
 const app = express()
 
+const jsonMiddleware = express.json()
+app.use(jsonMiddleware)
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/calendar.html')
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/movies', (req, res) => {
+  if (req.accepts('json')) {
+    const data = [
+      {id: 1, text: "Item 1"},
+      {id: 2, text: "Item 2"},
+    ]
+    res
+      .header({
+        'Content-Type': 'application/json',
+      })
+      .json(data)
+  } else {
+    res.sendStatus(406)
+  }
+})
+
+app.post('/movies', (req, res) => {
+  console.log(req.body)
 })
 
 app.use(
--- public/client.js
+++ public/client.js
@@ -1,45 +1,54 @@
-new Vue({
-    el: '#calendar-vue',
+{
+  Vue.component('todo-item', {
+    props: ['item'],
+    template: `
+      <li class="item"><h3>{{ item.title }}</h3></li>
+    `,
+  })
+
+  const vm = new Vue({
+    el: '#app',
+    created: function() {
+      const vm = this
+      // fetch('/movies')
+      //   .then(function(response) {
+      //     return response.json()
+      //   })
+      //   .then((myJson) => {
+      //     this
+      //     vm.title
+      //     console.log(JSON.stringify(myJson));
+      //   })
+
+      const data = {item: 1}
+      fetch('/movies', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        credentials: 'omit',
+        body: JSON.stringify(data),
+      })
+    },
     data: {
-        title: 'Titre Cal',
-        count: 0,
-        items: [1, 2, 3]
+      items: [{
+        title: "#0",
+      }],
+      title: 'Titre Calend.',
     },
     computed: {
-        computedVar: vm => vm.title + 'test'
+      count: function() {
+        return this.items.length
+      },
     },
     methods: {
-        handleClick: function() {
-            this.count++
-        }
-    }
-})
-
-{
-    const el = document.querySelector('#calendar')
-
-    const numDays = 7
-    const startDate = new Date()
-    const days = []
-    const dateAt = (date, i) =>
-        new Date(
-            new Date(startDate).setDate(
-                startDate.getDate() + numDays - 1 + i
-            )
-        )
-    for (let i = 0; i < numDays; i++) {
-        days.push(dateAt(startDate, i))
-    }
-    console.log(days)
-
-    el.innerHTML = `
-        <h1>Calendar</h1>
-        <ol class="rixo-calendar">
-            ${
-                days.map(day => `
-                    <li class="day">${day}</li>
-                `).join('')
-            }
-        </ol>
-    `
+      add: function() {
+        const index = this.items.length
+        console.trace()
+        this.items.push({
+          title: "#" + (index + 1),
+        })
+      },
+    },
+  })
 }
